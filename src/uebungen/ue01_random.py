192768ddd2b1eeadcbc34c976286b79e830d26b3
reidaz22 <reidaz22@htl-kaindorf.at>
2025-09-30 07:39:13 +0000

    Kommentare hinzugefügt

--- src/uebungen/ue01_random.py
+++ src/uebungen/ue01_random.py
@@ -3,4 +3,8 @@ import random
 liste = []
 for i in range(-10,10):
 	liste.append(random.randint(-10,10))
+# Variablen können in einer print Anweisung mit {} makiert und ausgegeben werden
+# Vor den " " muss allerdings ein f stehen, ansosten werdie die Variablen nicht erkannt
+# Ich kann natürlich ein eine print-Anweisung im C-Style schreiben
 print(f"Eine Liste mit {len(liste)} Zahlen: {liste}")
+
